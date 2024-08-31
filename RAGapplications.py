5bcefbc6b12e5c19ea241ce970683a4cbc89b527
Chhotoo-11 <110529750+Chhotoo-11@users.noreply.github.com>
2024-08-31 13:12:54 +0530

    Update RAGapplications.py

--- RAGapplications.py
+++ RAGapplications.py
@@ -23,6 +23,7 @@ from langchain_groq import ChatGroq
 #from langchain_chroma import Chroma
 from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
 from langchain_chroma.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -104,7 +105,7 @@ if api_key:
             # Split and create embeddings for the documents
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
             splits = text_splitter.split_documents(documents)
-            vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+            vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
             retriever = vectorstore.as_retriever() 
 
             contextualize_q_system_prompt=(
