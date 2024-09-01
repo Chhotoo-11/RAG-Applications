fde00ee69d64cf31746a67d8c816be50c9255e5b
Chhotoo-11 <110529750+Chhotoo-11@users.noreply.github.com>
2024-09-01 14:15:52 +0530

    Update RAGapplications.py

--- RAGapplications.py
+++ RAGapplications.py
@@ -21,7 +21,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
-from langchain_chroma.vectorstores import Chroma
+#from langchain_chroma.vectorstores import Chroma
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_community.vectorstores import FAISS
