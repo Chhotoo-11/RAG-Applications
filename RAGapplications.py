ce118e178ced807d8ae6d60f30a512a0255b869a
Chhotoo-11 <110529750+Chhotoo-11@users.noreply.github.com>
2024-09-03 14:08:35 +0530

    Update RAGapplications.py

--- RAGapplications.py
+++ RAGapplications.py
@@ -114,99 +114,69 @@ else:
                 vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
                 retriever = vectorstore.as_retriever() 
 
-                contextualize_q_system_prompt = (
-                    "Given a chat history and the latest user question "
+                contextualize_q_system_prompt=(
+                    "Given a chat history and the latest user question"
                     "which might reference context in the chat history, "
                     "formulate a standalone question which can be understood "
                     "without the chat history. Do NOT answer the question, "
                     "just reformulate it if needed and otherwise return it as is."
                 )
-
+        
                 contextualize_q_prompt = ChatPromptTemplate.from_messages(
-                    [
-                        ("system", contextualize_q_system_prompt),
-                        MessagesPlaceholder("chat_history"),
-                        ("human", "{input}"),
-                    ]
+                        [
+                            ("system", contextualize_q_system_prompt),
+                            MessagesPlaceholder("chat_history"),
+                            ("human", "{input}"),
+                        ]
                 )
-
-                history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
-
+        
+                history_aware_retriever= create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
+        
                 # Answer question
                 system_prompt = (
-                    "You are an assistant for question-answering tasks. "
-                    "Use the following pieces of retrieved context to answer "
-                    "the question. If you don't know the answer, say that you "
-                    "don't know. Use three sentences maximum and keep the "
-                    "answer concise. If the user asks for a summary, use the summarization function."
-                    "\n\n"
-                    "{context}"
-                )
+                        "You are an assistant for question-answering tasks. "
+                        "Use the following pieces of retrieved context to answer "
+                        "the question. If you don't know the answer, say that you "
+                        "don't know. Use three sentences maximum and keep the "
+                        "answer concise."
+                        "\n\n"
+                        "{context}"
+                    )
                 qa_prompt = ChatPromptTemplate.from_messages(
-                    [
-                        ("system", system_prompt),
-                        MessagesPlaceholder("chat_history"),
-                        ("human", "{input}"),
-                    ]
-                )
+                        [
+                            ("system", system_prompt),
+                            MessagesPlaceholder("chat_history"),
+                            ("human", "{input}"),
+                        ]
+                    )
                 
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-
-                def get_session_history(session: str) -> BaseChatMessageHistory:
-                    if session not in st.session_state.store:
-                        st.session_state.store[session] = ChatMessageHistory()
-                    return st.session_state.store[session]
+                question_answer_chain= create_stuff_documents_chain(llm,qa_prompt)
+                rag_chain= create_retrieval_chain(history_aware_retriever,question_answer_chain)
+        
+                def get_session_history(session: str)->BaseChatMessageHistory:
+                    if session_id not in st.session_state.store:
+                        st.session_state.store[session_id]= ChatMessageHistory()
+                    return st.session_state.store[session_id]
                 
-                conversational_rag_chain = RunnableWithMessageHistory(
-                    rag_chain,
-                    get_session_history,
+                conversational_rag_chain=RunnableWithMessageHistory(
+                    rag_chain,get_session_history,
                     input_messages_key="input",
                     history_messages_key="chat_history",
                     output_messages_key="answer"
                 )
-
-                # def get_summary(splits):
-                #     prompt_template = """
-                #     Provide a summary of the following content in 300 words:
-                #     Content:{text}
-                #     """
-                #     prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
-                #     chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=prompt, combine_prompt=prompt)
-                #     return chain.run(splits)
-
-                # def is_summary_request(query):
-                #     summary_keywords = ['summary', 'summarize', 'summarization', 'description', 'describe', 'overview', 'brief', 'briefly', 'digest', 'recap', 'outline']
-                #     return any(keyword in query.lower() for keyword in summary_keywords)
-
-
-
+        
                 user_input = st.text_input("Your question:")
                 if user_input:
-                    session_history = get_session_history(session_id)
+                    session_history=get_session_history(session_id)
                     response = conversational_rag_chain.invoke(
                         {"input": user_input},
-                         config={
-                                "configurable": {"session_id": session_id}
-                            },
-                        )
-                    st.success(f"Assistant: {response['answer']}")
-                    
-                    # # Check if the user is asking for a summary
-                    # if is_summary_request(user_input):
-                    #                 summary = get_summary(splits)
-                    #                 st.write("Assistant: Here's a summary of the document(s):")
-                    #                 st.success(summary)
-                    #     #session_history.add_user_message(user_input)
-                    #     #session_history.add_ai_message(summary)
-                    # else:
-                    #     response = conversational_rag_chain.invoke(
-                    #         {"input": user_input},
-                    #         config={
-                    #             "configurable": {"session_id": session_id}
-                    #         },
-                    #     )
-                    #     st.success(f"Assistant: {response['answer']}")
+                        config={
+                            "configurable": {"session_id":session_id}
+                        },  # constructs a key "abc123" in `store`.
+                    )
+                    st.write(st.session_state.store)
+                    st.write("Assistant:", response['answer'])
+                    st.write("Chat History:", session_history.messages)
             
             
 
